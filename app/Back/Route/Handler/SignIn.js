d88372a87cbea0dc40f91e88fe0bd3d09109ea6c
Alex Gusev <alex@flancer64.com>
2019-08-13 18:41:02 +0300

    WF-184 Save authentication token in cookie

--- app/Back/Route/Handler/SignIn.js
+++ app/Back/Route/Handler/SignIn.js
@@ -2,6 +2,10 @@
 const $Paseto = require('paseto.js');
 const $encoder = new $Paseto.V2();
 
+const _b64sk = "hNoAS1BVeOZkKYUt4Y5WcheSdu7L5DVsNmX14dKvADQ";
+const _pk = new $Paseto.PublicKey(new $Paseto.V2());
+
+
 /**
  * Authenticate users by username & password.
  *
@@ -34,20 +38,38 @@ function Fl64_Pilot_Back_Route_Handler_SignIn(
         let result = {msg: "U-ups... authentication is failed."};
         if (typeof body === "object" && body.user && body.password) {
             if (_base[body.user] === body.password) {
-                $encoder.symmetric()
-                    .then(sk => {
-                        // (sk instanceof Paseto.SymmetricKey) -> true
-                        const message = 'A screaming comes across the sky.'
-                        return $encoder.encrypt(message, sk);
+                // compose body token for authentication token
+                const message = JSON.stringify({user: body.user});
+                _pk.base64(_b64sk)
+                    .then(() => {
+                        const encoder = _pk.protocol();
+                        return encoder.encrypt(message, _pk);
                     })
                     .then(token => {
                         result = {token};
+                        const opts = {maxAge: 900000, httpOnly: true};
+                        res.cookie("teq-fw-auth", token, opts);
                         res.send(result);
                     })
                     .catch((err) => {
                         _logger.error(err);
                         res.send(result);
                     });
+
+                // $encoder.symmetric()
+                //     .then(sk => {
+                //         // (sk instanceof Paseto.SymmetricKey) -> true
+                //         const message = 'A screaming comes across the sky.'
+                //         return $encoder.encrypt(message, sk);
+                //     })
+                //     .then(token => {
+                //         result = {token};
+                //         res.send(result);
+                //     })
+                //     .catch((err) => {
+                //         _logger.error(err);
+                //         res.send(result);
+                //     });
             } else {
                 res.send(result);
             }
